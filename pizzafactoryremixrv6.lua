c8e213e7b711c58aa245a706ec67ee928548cf9b
CSWC <43013100+bigboi42069@users.noreply.github.com>
2024-10-19 03:48:44 -0230

    Updated pizzafactoryremixrv6.lua
    Updated.

--- pizzafactoryremixrv6.lua
+++ pizzafactoryremixrv6.lua
@@ -97,7 +97,7 @@ Layout=Create("UIGridLayout",settings_1,{VerticalAlignment="Center", SortOrder="
 cashier=Create("Frame",settings_1,{Name="cashier", LayoutOrder=4, BackgroundTransparency=1, Size=UDim2.new(0,100,0,100), BackgroundColor3=Color3.new(1,1,1)})
 Label=Create("TextLabel",cashier,{TextWrapped=true, Size=UDim2.new(0.6,0,1,0), Text="Cashier", TextSize=14, TextXAlignment="Left", Font="SourceSans", 
 	BackgroundTransparency=1, Position=UDim2.new(0.4,0,0,0), TextColor3=Color3.new(1,1,1), TextScaled=true, BackgroundColor3=Color3.new(1,1,1)})
-cashierBtn=Create("ImageButton",cashier,{Name="cashierBtn", ImageTransparency=1, BorderSizePixel=0, Size=UDim2.new(0.38,0,1,0), BackgroundColor3=Color3.new(0.444,0.555,0.444)})
+cashierBtn=Create("ImageButton",cashier,{Name="cashierBtn", ImageTransparency=1, BorderSizePixel=0, Size=UDim2.new(0.38,0,1,0), BackgroundColor3=Color3.new(0.333,0.333,0.333)})
 cashierSlider=Create("Frame",cashierBtn,{Name="slider", Size=UDim2.new(0.5,-4,1,-4), Position=UDim2.new(doCashier and 0.5 or 0,2,0,2), BorderSizePixel=0, BackgroundColor3=Color3.new(0.784,0.784,0.784)})
 kitchen=Create("Frame",settings_1,{Name="kitchen", LayoutOrder=9, BackgroundTransparency=1, Size=UDim2.new(0,100,0,100), BackgroundColor3=Color3.new(1,1,1)})
 Label_2=Create("TextLabel",kitchen,{TextWrapped=true, Size=UDim2.new(0.6,0,1,0), Text="Deliver At:", TextSize=14, TextXAlignment="Right", Font="SourceSans", 
@@ -117,26 +117,26 @@ refillAtBox=Create("TextBox",refillAt,{Name="refillAtBox", TextWrapped=true, Siz
 supplier=Create("Frame",settings_1,{Name="supplier", LayoutOrder=6, BackgroundTransparency=1, Size=UDim2.new(0,100,0,100), BackgroundColor3=Color3.new(1,1,1)})
 Label_5=Create("TextLabel",supplier,{TextWrapped=true, Size=UDim2.new(0.6,0,1,0), Text="Supplier", TextSize=14, TextXAlignment="Left", Font="SourceSans", 
 	BackgroundTransparency=1, Position=UDim2.new(0.4,0,0,0), TextColor3=Color3.new(1,1,1), TextScaled=true, BackgroundColor3=Color3.new(1,1,1)})
-supplierBtn=Create("ImageButton",supplier,{Name="supplierBtn", ImageTransparency=1, BorderSizePixel=0, Size=UDim2.new(0.38,0,1,0), BackgroundColor3=Color3.new(0.444,0.555,0.444)})
+supplierBtn=Create("ImageButton",supplier,{Name="supplierBtn", ImageTransparency=1, BorderSizePixel=0, Size=UDim2.new(0.38,0,1,0), BackgroundColor3=Color3.new(0.333,0.333,0.333)})
 supplierSlider=Create("Frame",supplierBtn,{Name="slider", Size=UDim2.new(0.5,-4,1,-4), Position=UDim2.new(doSupplier and 0.5 or 0,2,0,2), BorderSizePixel=0, BackgroundColor3=Color3.new(0.784,0.784,0.784)})
 delivery=Create("Frame",settings_1,{Name="delivery", LayoutOrder=5, BackgroundTransparency=1, Size=UDim2.new(0,100,0,100), BackgroundColor3=Color3.new(1,1,1)})
 Label_6=Create("TextLabel",delivery,{TextWrapped=true, Size=UDim2.new(0.6,0,1,0), Text="Delivery", TextSize=14, TextXAlignment="Left", Font="SourceSans", 
 	BackgroundTransparency=1, Position=UDim2.new(0.4,0,0,0), TextColor3=Color3.new(1,1,1), TextScaled=true, BackgroundColor3=Color3.new(1,1,1)})
-deliveryBtn=Create("ImageButton",delivery,{Name="deliveryBtn", ImageTransparency=1, BorderSizePixel=0, Size=UDim2.new(0.38,0,1,0), BackgroundColor3=Color3.new(0.444,0.555,0.444)})
+deliveryBtn=Create("ImageButton",delivery,{Name="deliveryBtn", ImageTransparency=1, BorderSizePixel=0, Size=UDim2.new(0.38,0,1,0), BackgroundColor3=Color3.new(0.333,0.333,0.333)})
 deliverySlider=Create("Frame",deliveryBtn,{Name="slider", Size=UDim2.new(0.5,-4,1,-4), Position=UDim2.new(doDelivery and 0.5 or 0,2,0,2), BorderSizePixel=0, BackgroundColor3=Color3.new(0.784,0.784,0.784)})
 boxer=Create("Frame",settings_1,{Name="boxer", LayoutOrder=2, BackgroundTransparency=1, Size=UDim2.new(0,100,0,100), BackgroundColor3=Color3.new(1,1,1)})
 boxerLbl=Create("TextLabel",boxer,{TextWrapped=true, Size=UDim2.new(0.6,0,1,0), Text="Boxer", TextSize=14, TextXAlignment="Left", Font="SourceSans", 
 	BackgroundTransparency=1, Position=UDim2.new(0.4,0,0,0), TextColor3=Color3.new(1,1,1), TextScaled=true, BackgroundColor3=Color3.new(1,1,1)})
-boxerBtn=Create("ImageButton",boxer,{Name="boxerBtn", ImageTransparency=1, BorderSizePixel=0, Size=UDim2.new(0.38,0,1,0), BackgroundColor3=Color3.new(0.444,0.555,0.444)})
+boxerBtn=Create("ImageButton",boxer,{Name="boxerBtn", ImageTransparency=1, BorderSizePixel=0, Size=UDim2.new(0.38,0,1,0), BackgroundColor3=Color3.new(0.333,0.333,0.333)})
 boxerSlider=Create("Frame",boxerBtn,{Name="slider", Size=UDim2.new(0.5,-4,1,-4), Position=UDim2.new(doBoxer and 0.5 or 0,2,0,2), BorderSizePixel=0, BackgroundColor3=Color3.new(0.784,0.784,0.784)})
 cook=Create("Frame",settings_1,{Name="cook", LayoutOrder=3, BackgroundTransparency=1, Size=UDim2.new(0,100,0,100), BackgroundColor3=Color3.new(1,1,1)})
 cookLbl=Create("TextLabel",cook,{TextWrapped=true, Size=UDim2.new(0.6,0,1,0), Text="Cook", TextSize=14, TextXAlignment="Left", Font="SourceSans", 
 	BackgroundTransparency=1, Position=UDim2.new(0.4,0,0,0), TextColor3=Color3.new(1,1,1), TextScaled=true, BackgroundColor3=Color3.new(1,1,1)})
-cookBtn=Create("ImageButton",cook,{Name="cookBtn", ImageTransparency=1, BorderSizePixel=0, Size=UDim2.new(0.38,0,1,0), BackgroundColor3=Color3.new(0.444,0.555,0.444)})
+cookBtn=Create("ImageButton",cook,{Name="cookBtn", ImageTransparency=1, BorderSizePixel=0, Size=UDim2.new(0.38,0,1,0), BackgroundColor3=Color3.new(0.333,0.333,0.333)})
 cookSlider=Create("Frame",cookBtn,{Name="slider", Size=UDim2.new(0.5,-4,1,-4), Position=UDim2.new(doCook and 0.5 or 0,2,0,2), BorderSizePixel=0, BackgroundColor3=Color3.new(0.784,0.784,0.784)})
 toggleAll=Create("Frame",settings_1,{Name="toggleAll", LayoutOrder=1, BackgroundTransparency=1, Size=UDim2.new(0,100,0,100), BackgroundColor3=Color3.new(1,1,1)})
 switch=Create("Frame",toggleAll,{Name="switch", BackgroundTransparency=1, Size=UDim2.new(0.75,0,1,0), BackgroundColor3=Color3.new(1,1,1)})
-allOffBtn=Create("ImageButton",switch,{Name="allOffBtn", ImageTransparency=1, BorderSizePixel=0, Size=UDim2.new(0.5,0,1,0), BackgroundColor3=Color3.new(0.444,0.555,0.444)})
+allOffBtn=Create("ImageButton",switch,{Name="allOffBtn", ImageTransparency=1, BorderSizePixel=0, Size=UDim2.new(0.5,0,1,0), BackgroundColor3=Color3.new(0.333,0.333,0.333)})
 allOnBtn=Create("ImageButton",switch,{Name="allOnBtn", ImageTransparency=1, BorderSizePixel=0, Size=UDim2.new(0.5,0,1,0), Position=UDim2.new(0.5,0,0,0), BackgroundColor3=Color3.new(0.666,0.333,0.333)})
 toggleAllSlider=Create("Frame",switch,{Name="slider", Size=UDim2.new(0.1,0,1,4), Position=UDim2.new(0.45,0,0,-2), BorderSizePixel=0, BackgroundColor3=Color3.new(0.784,0.784,0.784)})
 messageLbl=Create("TextLabel",topbar,{Name="messageLbl", Size=UDim2.new(0.5,0,1,0), Text="Saved.", TextSize=14, Font="GothamSemibold", BackgroundTransparency=1, 
@@ -149,10 +149,10 @@ centerCamBtn=Create("ImageButton",camframe,{Name="centerCamBtn", Image="rbxasset
 creditLbl=Create("TextLabel",main,{Position=UDim2.new(0,0,1,4),Size=UDim2.new(1,0,0,16),BackgroundTransparency=1,TextColor3=Color3.new(1,1,1),Text="by sirelKilla & BallsNDeath",TextScaled=true,TextStrokeTransparency=.8})
 
 local function toggleButtonColor(button)
-    if button.BackgroundColor3 == Color3.new(0.444,0.555,0.444) then
-        button.BackgroundColor3 = Color3.new(0.666, 0.333, 0.333)
+    if button.BackgroundColor3 == Color3.new(0.333,0.333,0.333) then
+        button.BackgroundColor3 = Color3.new(0.888,0.222,0.222)
     else
-        button.BackgroundColor3 = Color3.new(0.444,0.555,0.444)
+        button.BackgroundColor3 = Color3.new(0.333,0.333,0.333)
     end
 end
 
@@ -163,7 +163,6 @@ local function toggleCashier(bool)
 		doCashier = not doCashier
 	end
 	cashierSlider:TweenPosition(UDim2.new(doCashier and 0.5 or 0,2,0,2),nil,"Sine",0.1,true)
-	toggleButtonColor(cashierBtn)
 end
 local function toggleCook(bool)
 	if bool~=nil then
@@ -172,7 +171,6 @@ local function toggleCook(bool)
 		doCook = not doCook
 	end
 	cookSlider:TweenPosition(UDim2.new(doCook and 0.5 or 0,2,0,2),nil,"Sine",0.1,true)
-	toggleButtonColor(cookBtn)
 end
 local function toggleBoxer(bool)
 	if bool~=nil then
@@ -181,7 +179,6 @@ local function toggleBoxer(bool)
 		doBoxer = not doBoxer
 	end
 	boxerSlider:TweenPosition(UDim2.new(doBoxer and 0.5 or 0,2,0,2),nil,"Sine",0.1,true)
-	toggleButtonColor(boxerBtn)
 end
 local function toggleDelivery(bool)
 	if bool~=nil then
@@ -190,7 +187,6 @@ local function toggleDelivery(bool)
 		doDelivery = not doDelivery
 	end
 	deliverySlider:TweenPosition(UDim2.new(doDelivery and 0.5 or 0,2,0,2),nil,"Sine",0.1,true)
-	toggleButtonColor(deliveryBtn)
 end
 local function toggleSupplier(bool)
 	if bool~=nil then
@@ -199,27 +195,31 @@ local function toggleSupplier(bool)
 		doSupplier = not doSupplier
 	end
 	supplierSlider:TweenPosition(UDim2.new(doSupplier and 0.5 or 0,2,0,2),nil,"Sine",0.1,true)
-	toggleButtonColor(supplierBtn)
 end
 
 cashierBtn.MouseButton1Click:Connect(function()
     toggleCashier()
+	toggleButtonColor(cashierBtn)
 end)
 
 cookBtn.MouseButton1Click:Connect(function()
     toggleCook()
+	toggleButtonColor(cookBtn)
 end)
 
 boxerBtn.MouseButton1Click:Connect(function()
     toggleBoxer()
+	toggleButtonColor(boxerBtn)
 end)
 
 deliveryBtn.MouseButton1Click:Connect(function()
     toggleDelivery()
+	toggleButtonColor(deliveryBtn)
 end)
 
 supplierBtn.MouseButton1Click:Connect(function()
     toggleSupplier()
+	toggleButtonColor(supplierBtn)
 end)
 
 allOffBtn.InputBegan:Connect(function()
@@ -234,11 +234,11 @@ allOffBtn.InputBegan:Connect(function()
 		if toggleAllSlider.Position.X.Scale<.01 then
 			toggleAllSlider:TweenPosition(UDim2.new(0.45,0,0,-2),nil,"Sine",0.1,true)
 		end
-			cashierBtn.BackgroundColor3 = Color3.new(0.444,0.555,0.444)
-			cookBtn.BackgroundColor3 = Color3.new(0.444,0.555,0.444)
-			boxerBtn.BackgroundColor3 = Color3.new(0.444,0.555,0.444)
-			deliveryBtn.BackgroundColor3 = Color3.new(0.444,0.555,0.444)
-			supplierBtn.BackgroundColor3 = Color3.new(0.444,0.555,0.444)
+			cashierBtn.BackgroundColor3 = Color3.new(0.333,0.333,0.333)
+			cookBtn.BackgroundColor3 = Color3.new(0.333,0.333,0.333)
+			boxerBtn.BackgroundColor3 = Color3.new(0.333,0.333,0.333)
+			deliveryBtn.BackgroundColor3 = Color3.new(0.333,0.333,0.333)
+			supplierBtn.BackgroundColor3 = Color3.new(0.333,0.333,0.333)
 	end
 end)
 allOnBtn.InputBegan:Connect(function()
@@ -253,11 +253,11 @@ allOnBtn.InputBegan:Connect(function()
 		if toggleAllSlider.Position.X.Scale>.88 then
 			toggleAllSlider:TweenPosition(UDim2.new(0.45,0,0,-2),nil,"Sine",0.1,true)
 		end
-			cashierBtn.BackgroundColor3 = Color3.new(0.666, 0.333, 0.333)
-			cookBtn.BackgroundColor3 = Color3.new(0.666, 0.333, 0.333)
-			boxerBtn.BackgroundColor3 = Color3.new(0.666, 0.333, 0.333)
-			deliveryBtn.BackgroundColor3 = Color3.new(0.666, 0.333, 0.333)
-			supplierBtn.BackgroundColor3 = Color3.new(0.666, 0.333, 0.333)
+			cashierBtn.BackgroundColor3 = Color3.new(0.888, 0.222, 0.222)
+			cookBtn.BackgroundColor3 = Color3.new(0.888, 0.222, 0.222)
+			boxerBtn.BackgroundColor3 = Color3.new(0.888, 0.222, 0.222)
+			deliveryBtn.BackgroundColor3 = Color3.new(0.888, 0.222, 0.222)
+			supplierBtn.BackgroundColor3 = Color3.new(0.888, 0.222, 0.222)
 	end
 end)
 local oldRefillAt=refillAtBox.Text
@@ -375,9 +375,12 @@ local function FindFirstCustomer()
 				elseif dialog:find("cheese",1,true) then
 					order = "CheesePizza"
 				end
-				pcall(function()
-                    c.HumanoidRootPart.CFrame = CFrame.new(50.30, -10, 83.24)
-                end)
+				if ffc(c,"Head") and ffc(c,"Humanoid") and c.Head.CFrame.Z<102 and ffc(c.Head,"Dialog") and ffc(c.Head.Dialog,"Correct") and ((c.Humanoid.SeatPart and c.Humanoid.SeatPart.Anchored) or (c.Humanoid.SeatPart==nil and (c.Head.Velocity.Z^2)^.5<.0001)) then
+					wait(0.02)
+					pcall(function()
+                    	c.HumanoidRootPart.CFrame = CFrame.new(50.30, -10, 83.24)
+                	end)
+				end
 				return c,order
 			end
 		else
