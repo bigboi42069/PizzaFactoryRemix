0ba0ed761a2c32aae8d77fd5edc4e1f4a59e813b
CSWC <43013100+bigboi42069@users.noreply.github.com>
2024-10-23 18:18:55 -0230

    Updated HalloweenFactory.lua
    Updated.

--- HalloweenFactory.lua
+++ HalloweenFactory.lua
@@ -1,6 +1,6 @@
 -- Made by Zingorr, Added to PizzaFactoryRemix by CSWC - 10/23/24
 wait(1)
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-30.9692764, 7.01014566, -443.712738, -0.0368146226, -1.18420891e-08, 0.999322116, 3.6141059e-09, 1, 1.19832642e-08, -0.999322116, 4.05281542e-09, -0.0368146226)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(199.163635, 7.01033974, -460.003845)
 
 local player = game.Players.LocalPlayer
 local character = player.Character or player.CharacterAdded:Wait()
@@ -8,22 +8,22 @@ local ffc = game.FindFirstChild
 
 -- **Rough** List of houses with their CFrame coordinates
 local houses = {
+    CFrame.new(199.163635, 7.01033974, -460.003845), -- A1 house
+    CFrame.new(206.516586, 7.00039196, -652.140869), -- A2 house
+    CFrame.new(206.499451, 7.00038719, -824.087158), -- A3 house
     CFrame.new(-30.9692764, 7.01014566, -443.712738), -- B1 house
     CFrame.new(40.7544098, 7.00028086, -475.953979), -- B2 house
     CFrame.new(-38.2014313, 7.00008202, -615.995056), -- B3 house
     CFrame.new(34.1237526, 7.01029873, -651.582153), -- B4 house
     CFrame.new(-42.5083694, 6.76008797, -790.086792), -- B5 house
     CFrame.new(40.8492317, 7.00034046, -835.990112), -- B6 house
-    CFrame.new(199.163635, 7.01033974, -460.003845), -- A1 house
-    CFrame.new(206.516586, 7.00039196, -652.140869), -- A2 house
-    CFrame.new(206.499451, 7.00038719, -824.087158), -- A3 house
     CFrame.new(-202.378128, 6.99999475, -435.736633), -- C1 house
     CFrame.new(-202.514328, 7.00004816, -621.020203), -- C2 house
     CFrame.new(-206.497513, 6.76011705, -789.92749) -- C3 house
 }
 
 local function typeofHouse(houseId)
-    local houseObj1 = workspace.Houses:FindFirstChild(houseId) -- Assuming house names like "House1", "House2", etc.
+    local houseObj1 = workspace.Houses:FindFirstChild(houseId)
     if houseObj1 then
         local houseObj2 = houseObj1:FindFirstChild(houseObj1)
         if houseObj2 then
@@ -59,7 +59,7 @@ local function teleportToHouses()
             warn("House not found: House" .. h)
         end
         
-        wait(8) -- Wait for 8 seconds before moving to the next house
+        wait(8) -- Wait for 8 seconds
     end
 end
 
