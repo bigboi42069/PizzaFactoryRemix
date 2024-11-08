f5f068c453f1f686af18252a926cd689d7355e64
CSWC <43013100+bigboi42069@users.noreply.github.com>
2024-11-08 05:40:19 -0330

    Updated pizzafactoryremixrv7.lua
    Updated.

--- pizzafactoryremixrv7.lua
+++ pizzafactoryremixrv7.lua
@@ -2,87 +2,92 @@
 getupvalues = getupvalues or debug.getupvalues
 setupvalue = setupvalue or debug.setupvalue
 if not (getrawmetatable and getupvalues and setupvalue and (getreg or debug.getregistry)) then
-	local h = Instance.new("Hint",workspace)
-	h.Text = "Executor Error."
-	wait(10)
-	h:Destroy()
-	return
+    local h = Instance.new("Hint", workspace)
+    h.Text = "Executor Error."
+    wait(10)
+    h:Destroy()
+    return
 end
+
 local settings = {refill_at=45, refill_end=60, deliver_at=8, stay_in_kitchen=true}
-local doCashier,doBoxer,doCook,doSupplier,doDelivery = false,false,false,false,false
+local doCashier, doBoxer, doCook, doSupplier, doDelivery = false, false, false, false, false
+
 if readfile then
-	pcall(function()
-		local new = game:GetService("HttpService"):JSONDecode(readfile("PizzaFarm.txt"))
-		local doOverwrite=false
-		for k,v in pairs(new) do
-			if settings[k]==nil then
-				doOverwrite=true
-				new[k]=nil
-			end
-		end
-		for k,v in pairs(settings) do
-			if new[k]==nil then
-				doOverwrite=true
-				new[k]=v
-			end
-		end
-		if doOverwrite then
-			warn("Settings overwritten")
-			writefile("PizzaFarm.txt",game:GetService("HttpService"):JSONEncode(new))
-		end
-		settings = new
-	end)
+    pcall(function()
+        local new = game:GetService("HttpService"):JSONDecode(readfile("PizzaFarm.txt"))
+        local doOverwrite = false
+        for k, v in pairs(new) do
+            if settings[k] == nil then
+                doOverwrite = true
+                new[k] = nil
+            end
+        end
+        for k, v in pairs(settings) do
+            if new[k] == nil then
+                doOverwrite = true
+                new[k] = v
+            end
+        end
+        if doOverwrite then
+            warn("Settings overwritten")
+            writefile("PizzaFarm.txt", game:GetService("HttpService"):JSONEncode(new))
+        end
+        settings = new
+    end)
 end
 
 if getconnections then
-	for _,c in next,getconnections(game:GetService("ScriptContext").Error) do
-		c:Disable()
-	end
+    for _, c in next, getconnections(game:GetService("ScriptContext").Error) do
+        c:Disable()
+    end
 end
 
 local player = game:GetService("Players").LocalPlayer
 local ffc = game.FindFirstChild
 local RNG = Random.new()
 local network
-local character,root,humanoid
-do
-	local reg = (getreg or debug.getregistry)()
-	for i=1,#reg do
-		local f = reg[i]
-		if type(f)=="function" then
-			for k,v in next,getupvalues(f) do
-				if typeof(v)=="Instance" then
-					if v.Name=="CashOut" then
-						setupvalue(f,k,{MouseButton1Click={wait=function()end,Wait=function()end}})
-					end
-				end
-			end
-			if tostring(getfenv(f)) == "Music" then
-				local consts = getconstants(f)
-				local loc=false
-				for ci,c in next,consts do
-					if c == "location changed" then
-						loc=true
-					elseif loc and c == "SendData" then
-						setconstant(f,ci,"ExplodeString")
-						break
-					end
-				end
-			end
-		elseif type(f)=="table" and rawget(f,"FireServer") and rawget(f,"BindEvents") then
-			network = f
-		end
-	end
-end
-assert(network,"failed to find network")
 
-Create = function(class,parent,props)
-	local new = Instance.new(class)
-	for k,v in next,props do
-		new[k]=v
-	end
-	new.Parent = parent
-	return new
+local character, root, humanoid
+do
+    local reg = (getreg or debug.getregistry)()
+    for i = 1, #reg do
+        local f = reg[i]
+        if type(f) == "function" then
+            local upvalues = getupvalues(f)
+            for k, v in next, upvalues do
+                if typeof(v) == "Instance" then
+                    if v.Name == "CashOut" then
+                        setupvalue(f, k, {MouseButton1Click = {wait = function() end, Wait = function() end}})
+                    end
+                end
+            end
+            if tostring(getfenv(f)) == "Music" then
+                local consts = getconstants(f)
+                local loc = false
+                for ci, c in next, consts do
+                    if c == "location changed" then
+                        loc = true
+                    elseif loc and c == "SendData" then
+                        setconstant(f, ci, "ExplodeString")
+                        break
+                    end
+                end
+            end
+        elseif type(f) == "table" and rawget(f, "FireServer") and rawget(f, "BindEvents") then
+            network = f
+        end
+    end
+end
+
+assert(network, "failed to find network")
+
+Create = function(class, parent, props)
+    local new = Instance.new(class)
+    for k, v in next, props do
+        new[k] = v
+    end
+    new.Parent = parent
+    return new
 end
 gui=Create("ScreenGui",game.CoreGui,{Name="Farm", ZIndexBehavior="Sibling"})
 main=Create("Frame",gui,{Name="main", Draggable=true, Active=true, Size=UDim2.new(0,350,0,100), Position=UDim2.new(.335,0,0.02,0), BackgroundColor3=Color3.new(0.098,0.098,0.098)})
